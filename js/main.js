d88a088d36926bf8b99fa779b44331018b22e32f
Sourita Ray <souritaray@gmail.com>
2023-02-09 12:06:38 +0530

    improvement of entire code-redone

--- js/main.js
+++ js/main.js
@@ -1,177 +1,84 @@
-let user_choice="";
-let comp_arr=document.querySelectorAll(".choice");
-let comp_choice="";
+const compScore = document.querySelector("#compScoreVal");
+const userScore = document.querySelector("#userScoreVal");
+const resultUser = document.querySelector("#result-user-stat");
+const resultComp = document.querySelector("#result-comp-stat");
+const result = document.querySelector("#result-final-stat");
+const all=document.querySelector(".choices");
+const images = document.querySelectorAll(".choice");
+let f=0;
+const res=document.createElement("span");
+result.appendChild(res);
+const user = document.createElement("img");
+user.className = "userImg";
+resultUser.appendChild(user);
+const compChoice = document.createElement("img");
+compChoice.className = "compImg";
+resultComp.appendChild(compChoice);
+let u=0;
 let c=0;
-let span=document.createElement("span");
-document.querySelector("#result-final-stat").appendChild(span);
-
-// Creating photo of rock & duplicating it to use in different places//
-let rockpic=document.createElement("img");
-rockpic.src=document.querySelector("div#r img").src;
-rockpic.style.width="30px";
-rockpic.style.marginLeft="10px";
-let cln=rockpic.cloneNode(true);
-
-// Creating photo of paper & duplicating it to use in different places//
-let paperpic=document.createElement("img");
-paperpic.src=document.querySelector("div#p img").src;
-paperpic.style.width="30px";
-paperpic.style.marginLeft="10px";
-let paper_cln=paperpic.cloneNode(true);
-
-// Creating photo of scissor & duplicating it to use in different places//
-let scissorpic=document.createElement("img");
-scissorpic.src=document.querySelector("div#s img").src;
-scissorpic.style.width="30px";
-scissorpic.style.marginLeft="10px";
-let scissor_cln=scissorpic.cloneNode(true);
-
-// User clicking rock
-document.querySelector("#r").addEventListener("click",function(){
-   user_choice="r";
-   let gg=comp_arr[Math.floor(Math.random()*comp_arr.length)];
-   comp_choice=gg.id;
-   c=0;
-   rockpic.style.display="none";
-   paperpic.style.display="none";
-   scissorpic.style.display="none";
-   cln.style.display="none";
-   paper_cln.style.display="none";
-   scissor_cln.style.display="none";
-   document.querySelector("#result-user-stat").insertAdjacentElement('beforeend',rockpic);
-   gamefunc1(user_choice,comp_choice);
-   
+images.forEach((e) => {  
+  e.setAttribute("onclick", `theChoices(${e.id})`);
 });
+function theChoices(e) {
+    f=f+1;
+    if(f>5){
+         if(c>u)
+            res.textContent="You Lost!";
+            else if(u>c)
+            res.textContent="You Won!";
+            else
+            res.textContent="Tie!";
 
-// User clicking paper
-document.querySelector("#p").addEventListener("click",function(){
-   user_choice="p";
-   c=0;
-   let gg=comp_arr[Math.floor(Math.random()*comp_arr.length)];
-   comp_choice=gg.id;
-   rockpic.style.display="none";
-   paperpic.style.display="none";
-   scissorpic.style.display="none";
-   paper_cln.style.display="none";
-   scissor_cln.style.display="none";
-   cln.style.display="none";
-   document.querySelector("#result-user-stat").insertAdjacentElement("beforeend",paperpic);
-   gamefunc2(user_choice,comp_choice);
-});
-
-// User clicking scissor
-document.querySelector("#s").addEventListener("click",function(){
-   user_choice="s";
-   c=0;
-   let gg=comp_arr[Math.floor(Math.random()*comp_arr.length)];
-   comp_choice=gg.id;
-   rockpic.style.display="none";
-   paperpic.style.display="none";
-   scissorpic.style.display="none";
-   scissor_cln.style.display="none";
-   cln.style.display="none";
-   paper_cln.style.display="none";
-   document.querySelector("#result-user-stat").insertAdjacentElement("beforeend",scissorpic);
-   gamefunc3(user_choice,comp_choice);
-});
-
-//***********ROCK************//
-
-
-function gamefunc1(user_choice,comp_choice){
-  if(user_choice=="r"){
-   
-   if(comp_choice=="p"){
-      document.querySelector("#userScoreVal").textContent="0";
-      document.querySelector("#compScoreVal").textContent="1";
-      rockpic.style.display="inline";
-      paperpic.style.display="inline";
-     document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",paperpic);
-      span.textContent="Computer";
-   }
-   else if(comp_choice=="s"){
-      document.querySelector("#userScoreVal").textContent="1";
-      document.querySelector("#compScoreVal").textContent="0";
-      rockpic.style.display="inline";
-      scissorpic.style.display="inline";
-      document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",scissorpic);
-      span.textContent="User";
-      
-   }
-   else{  
-      document.querySelector("#userScoreVal").textContent="0";
-      document.querySelector("#compScoreVal").textContent="0";
-      rockpic.style.display="inline";
-      cln.style.display="inline";
-      document.querySelector("#result-comp-stat").insertAdjacentElement('beforeend',cln); 
-      span.textContent="Tie";
-   }
-  }
+        let replay=document.createElement("button");
+        replay.innerText="REPLAY";
+        replay.style.backgroundColor="#ff6666";
+        replay.style.padding="5px";
+        replay.style.marginLeft="47%";
+        all.replaceWith(replay);
+        c=0;
+        u=0;
+        compScore.innerHTML=c;
+        userScore.innerHTML=u;
+        f=0;
+        replay.addEventListener("click",()=>{
+            replay.replaceWith(all);
+            res.textContent="";
+            user.src="";
+            compChoice.src="";
+            e="";
+            isWinner(f,e)
+        });
+    }
+    else{
+             isWinner(f,e);
+        }
+    
 }
-
-//******PAPER*******//
-
-function gamefunc2(user_choice,comp_choice){
-   if(user_choice=="p"){
-      
-      if(comp_choice=="r"){
-         document.querySelector("#userScoreVal").textContent="1";
-         document.querySelector("#compScoreVal").textContent="0";
-        paperpic.style.display="inline";
-      rockpic.style.display="inline";
-      document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",rockpic);
-      span.textContent="User";
-         
-      }
-      else if(comp_choice=="s"){
-         document.querySelector("#userScoreVal").textContent="0";
-         document.querySelector("#compScoreVal").textContent="1";
-         paperpic.style.display="inline";
-         scissorpic.style.display="inline";
-         document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",scissorpic);
-         span.textContent="Computer";
-      }
-      else{
-         document.querySelector("#userScoreVal").textContent="0";
-         document.querySelector("#compScoreVal").textContent="0";
-         paperpic.style.display="inline";
-         paper_cln.style.display="inline";
-         document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",paper_cln);
-         span.textContent="Tie";
-      }
-     }
-   
+function computerChoice(){
+   let compChoiceImg= images[Math.floor(Math.random()*3)];
+   return compChoiceImg;
 }
+function isWinner(f,e){
+user.src=e.children[0].src;
+    let computer=computerChoice();
+    compChoice.src= computer.children[0].src;
+    switch(e.id+computer.id){
+        case "sp":
+        case "rs":
+        case "pr":
+            compScore.innerHTML=c;
+            userScore.innerHTML=++u;
+            break;
 
-//*****SCISSOR******//
-  
-function gamefunc3(user_choice,comp_choice){
-   if(user_choice="s"){
-      
-      if(comp_choice=="r"){
-         document.querySelector("#userScoreVal").textContent="0";
-         document.querySelector("#compScoreVal").textContent="1";
-         scissorpic.style.display="inline";
-         rockpic.style.display="inline";
-         document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",rockpic);
-         span.textContent="Computer";
-         
-      }
-      else if(comp_choice=="p"){
-         document.querySelector("#userScoreVal").textContent="1";
-         document.querySelector("#compScoreVal").textContent="0";
-         scissorpic.style.display="inline";
-         paperpic.style.display="inline";
-         document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",paperpic);
-         span.textContent="User";
-      }
-      else{
-         document.querySelector("#userScoreVal").textContent="0";
-         document.querySelector("#compScoreVal").textContent="0";
-         scissorpic.style.display="inline";
-         scissor_cln.style.display="inline";
-         document.querySelector("#result-comp-stat").insertAdjacentElement("beforeend",scissor_cln);
-         span.textContent="Tie";
-      }
-     }
+        case "ps":
+        case "sr":
+        case "rp":
+            compScore.innerHTML=++c;
+            userScore.innerHTML=u;
+            break;
+        default:
+            compScore.innerHTML=c;
+            userScore.innerHTML=u;
+            break;
+    }
 }
